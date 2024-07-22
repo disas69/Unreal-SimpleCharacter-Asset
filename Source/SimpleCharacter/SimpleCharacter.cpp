23ba34394d677dc53a8da348f6e71e45c4ca347d
Eugene <evgenii.esaulenko@gmail.com>
2024-07-22 15:18:41 +0200

    Initial commit

---
+++ Source/SimpleCharacter/SimpleCharacter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleCharacter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleCharacter, "SimpleCharacter" );
